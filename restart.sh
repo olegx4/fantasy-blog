e6965fc167873bcc68ff15e5d08b972fc5cf317f
olegx4 <olegx4@ukr.net>
2020-02-18 23:53:25 +0200

    Added dependencies for postgres, modified restart.sh

--- restart.sh
+++ restart.sh
@@ -1,2 +1,2 @@
 docker rm -f fantasy-blog-db
-docker run -d -v /home/fantasy-blog:/var/lib/postgresql/data --name fantasy-blog-db -e POSTGRES_DB=fantasy-blog -p 5432:5432 postgres
+docker run -d -v /home/fantasy-blog:/var/lib/postgresql/data --name fantasy-blog-db -e POSTGRES_DB=fantasy-blog -e POSTGRES_HOST_AUTH_METHOD=trust -p 5432:5432 postgres
