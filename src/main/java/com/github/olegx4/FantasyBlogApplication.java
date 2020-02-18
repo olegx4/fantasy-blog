9daf20e55739b2081b06fb4e48de91e06849c6b5
olegx4 <olegx4@ukr.net>
2020-02-18 21:56:47 +0200

    Initial commit

---
+++ src/main/java/com/github/olegx4/FantasyBlogApplication.java
@@ -0,0 +1,11 @@
+package com.github.olegx4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasyBlogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FantasyBlogApplication.class, args);
+    }
+}
