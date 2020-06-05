f8f351bf968b6944352c188899c16a447e6ba1a4
Oleh <olegx4@ukr.net>
2020-06-05 23:47:20 +0300

    Added script for starting app local db and app from dockerhub.

---
+++ net_restart.sh
@@ -0,0 +1,6 @@
+docker-compose down
+docker stop f-b-app
+docker rm f-b-app
+docker-compose up -d
+docker run -d --name f-b-app --network fantasyblog_blog-db-network -e DB_HOST=blogdb -e DB_PORT=5432  -p 8000:8080 --name f-b-app olegx4/f-b-app:0.0.1
+docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' f-b-app
