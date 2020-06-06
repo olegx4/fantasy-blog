d70db6af61f2474b865c8a2803217ecde202f2b3
Oleh <olegx4@ukr.net>
2020-06-06 23:53:41 +0300

    Added settings of db image to net_restart.sh script; changed location of local db-files

--- net_restart.sh
+++ net_restart.sh
@@ -1,7 +1,9 @@
-docker-compose down
 docker pull olegx4/f-b-app:0.0.1
+docker pull postgres
+docker stop blogdb
 docker stop f-b-app
+docker rm blogdb
 docker rm f-b-app
-docker-compose up -d
+docker run -d --name blogdb --network fantasyblog_blog-db-network -v "/blogdb:/var/lib/postgresql/data" -e POSTGRES_DB=blog -e POSTGRES_PASSWORD=postgres -e POSTGRES_USER=postgres -p 5432:5432 postgres
 docker run -d --name f-b-app --network fantasyblog_blog-db-network -e DB_HOST=blogdb -e DB_PORT=5432  -p 8000:8080 --name f-b-app olegx4/f-b-app:0.0.1
 docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' f-b-app
