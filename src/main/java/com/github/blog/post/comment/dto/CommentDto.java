7178dfecb95405597d02869ef2b9bf2397383132
Oleh <olegx4@ukr.net>
2020-06-08 00:00:19 +0300

    Added getters for comments fields in comment dto

--- src/main/java/com/github/blog/post/comment/dto/CommentDto.java
+++ src/main/java/com/github/blog/post/comment/dto/CommentDto.java
@@ -5,13 +5,25 @@ import com.github.blog.post.comment.Comment;
 import java.time.Instant;
 
 public class CommentDto {
-    private Long id;
-    private String text;
-    private Instant createdAt;
+    private final Long id;
+    private final String text;
+    private final Instant createdAt;
 
     public CommentDto(Comment comment) {
         this.id = comment.getId();
         this.text = comment.getText();
         this.createdAt = comment.getCreatedAt();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
     }
 }
