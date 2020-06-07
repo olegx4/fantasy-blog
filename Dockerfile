9ca53b1f776e2d0f1b03e2f1500ae588282f0f5d
Oleh <olegx4@ukr.net>
2020-06-08 01:28:18 +0300

    Changed Dockerfile and configured it to build app from resources (e.g local files or files in repository) Configured script net_restart.sh for pulling and running the "latest" version of app

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,10 @@
+FROM gradle:6.2.1-jdk11 as builder
+COPY --chown=gradle:gradle . /home/gradle/src
+WORKDIR /home/gradle/src
+RUN gradle build --no-daemon
+
 FROM openjdk:11-slim
-VOLUME /tmp
-ARG JAR_FILE=build/libs/fantasy-blog-0.0.1-SNAPSHOT.jar
-ADD ${JAR_FILE} springapp.jar
-ENTRYPOINT ["java", "-Djava.security.egd=file:/dev/./urandom", "-jar", "/springapp.jar"]
+RUN mkdir /app
+COPY --from=builder /home/gradle/src/build/libs/fantasy-blog-0.0.1-SNAPSHOT.jar /app/springapp.jar
+
+ENTRYPOINT ["java", "-Djava.security.egd=file:/dev/./urandom", "-jar", "/app/springapp.jar"]
--- net_restart.sh
+++ net_restart.sh
@@ -1,9 +1,9 @@
-docker pull olegx4/f-b-app:0.0.1
+docker pull olegx4/f-b-app
 docker pull postgres
 docker stop blogdb
 docker stop f-b-app
 docker rm blogdb
 docker rm f-b-app
 docker run -d --name blogdb --network fantasyblog_blog-db-network -v "/blogdb:/var/lib/postgresql/data" -e POSTGRES_DB=blog -e POSTGRES_PASSWORD=postgres -e POSTGRES_USER=postgres -p 5432:5432 postgres
-docker run -d --name f-b-app --network fantasyblog_blog-db-network -e DB_HOST=blogdb -e DB_PORT=5432  -p 8000:8080 --name f-b-app olegx4/f-b-app:0.0.1
+docker run -d --name f-b-app --network fantasyblog_blog-db-network -e DB_HOST=blogdb -e DB_PORT=5432  -p 8000:8080 --name f-b-app olegx4/f-b-app
 docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' f-b-app
