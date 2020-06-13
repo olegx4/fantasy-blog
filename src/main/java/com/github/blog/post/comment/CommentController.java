1f513a765a80b601c5bdea8940d571d78ba640cb
Oleh <olegx4@ukr.net>
2020-06-14 00:12:01 +0300

    Added validation of commentCommand parameters.

--- src/main/java/com/github/blog/post/comment/CommentController.java
+++ src/main/java/com/github/blog/post/comment/CommentController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -25,7 +26,7 @@ public class CommentController {
     }
 
     @PostMapping("/{postId}/comments")
-    public ResponseEntity<CommentDto> addComment(@PathVariable Long postId, @RequestBody CommentCommand command) {
+    public ResponseEntity<CommentDto> addComment(@PathVariable Long postId, @Valid @RequestBody CommentCommand command) {
         return new ResponseEntity<>(commentService.addComment(postId, command), HttpStatus.CREATED);
     }
 }
