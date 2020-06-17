290532cd6e8ee6119dce003c73bdf409a4c970b5
Oleh <olegx4@ukr.net>
2020-06-17 21:11:30 +0300

    Refactored CommentUpdateCommand

--- src/main/java/com/github/blog/post/comment/dto/command/CommentUpdateCommand.java
+++ src/main/java/com/github/blog/post/comment/dto/command/CommentUpdateCommand.java
@@ -3,6 +3,9 @@ package com.github.blog.post.comment.dto.command;
 import javax.validation.constraints.NotBlank;
 
 public class CommentUpdateCommand {
+    @NotBlank
+    private String text;
+
     public String getText() {
         return text;
     }
@@ -11,7 +14,4 @@ public class CommentUpdateCommand {
         this.text = text;
         return this;
     }
-
-    @NotBlank
-    private String text;
 }
